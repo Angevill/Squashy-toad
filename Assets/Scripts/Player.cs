bd006387cc78137dfadbf54599c18a5e1186ec78
unknown <>
2016-05-01 11:32:29 +0800

    Procedurally generating terrain,also fix bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class Player : MonoBehaviour {
 
-    public Text gazeText;
     public float jumpAngleInDegree = 30;
     public float jumpSpeed = 3;
 
     private CardboardHead gazeHead;
     private Rigidbody gazeRigid;
     private bool onFloor;
+    private float lastJumpRequestTime = 0.0f;
     
 
 	// Use this for initialization
@@ -23,34 +24,35 @@ public class Player : MonoBehaviour {
 
     private void PullTrigger()
     {
-        if (onFloor)
+        RequestJump();
+    }
+
+    private void RequestJump()
+    {
+        lastJumpRequestTime = Time.time;
+        gazeRigid.WakeUp();
+    }
+
+    private void Jump()
+    {
+        float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
+        Vector3 projectedVector = Vector3.ProjectOnPlane(gazeHead.Gaze.direction, Vector3.up);
+        Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+        gazeRigid.velocity = jumpVector * jumpSpeed;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        float delta = Time.time - lastJumpRequestTime;
+        if(delta < 0.1f)
         {
-            float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
-            Vector3 projectedVector = Vector3.ProjectOnPlane(gazeHead.Gaze.direction, Vector3.up);
-            Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
-            gazeRigid.velocity = jumpVector * jumpSpeed;
+            Jump();
+            lastJumpRequestTime = 0.0f;
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        onFloor = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        onFloor = false;
     }
 
     // Update is called once per frame
     void Update () {
-        if (gazeText != null)
-        {
-            gazeText.text = gazeHead.Gaze.ToString();
-        }
-        else
-        {
-            print("Dont forget to pick text object!!");
-        }
+
 	}
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
     public GameObject[] lanePrefabs;
+    public float spawnHorizon = 500f;
+    public float laneWidth = 2f;
+    public GameObject player;
+    public Transform spawnerParent;
+
+    private float nextLaneOffset = 0f;
 
 	// Use this for initialization
 	void Start () {
-        GameObject lane = lanePrefabs[0];
-        Instantiate(lane, Vector3.zero, Quaternion.identity);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float playerForwardPos = player.transform.position.z;
+        while (playerForwardPos + spawnHorizon > nextLaneOffset)
+        {
+            int randomLaneIndex = Random.Range(0, lanePrefabs.Length);
+            GameObject laneItem = lanePrefabs[randomLaneIndex];
+            Vector3 nextLanePos = nextLaneOffset * Vector3.forward;
+            GameObject laneHolder = Instantiate(laneItem, nextLanePos, Quaternion.identity) as GameObject;
+            laneHolder.transform.parent = spawnerParent;
+            nextLaneOffset += laneWidth;
+        }
+    }
 }
