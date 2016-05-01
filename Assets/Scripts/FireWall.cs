5013a56200f0d7f011e1494609a3e7ec224dde13
Angevill <angevill20006cn@gmail.com>
2016-05-01 19:10:29 +0800

    Add splash screen and exit event

---
+++ Assets/Scripts/FireWall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FireWall : MonoBehaviour {
+    public float creepSpeed = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        FollowPlayer();
+        CreepForward();
+    }
+
+    private void CreepForward()
+    {
+        transform.position += Vector3.forward * creepSpeed;
+    }
+
+    private void FollowPlayer()
+    {
+        GameObject player = GameObject.Find("CardboardPlayer");
+        Vector3 deltaVector = player.transform.position - transform.position;
+        Vector3 projectedDeltaVector = Vector3.Project(deltaVector, Vector3.left);
+        transform.position += projectedDeltaVector;
+    }
+}
---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Request:");
+        SceneManager.LoadScene("Main");
+    }
+}
